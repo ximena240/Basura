6b625475ea94a22bd4117d27b740ccf257a8f915
ximena240 <tijerina.almaguer.22067@itsmante.edu.mx>
2025-06-03 07:53:03 -0600

    otro

--- src/pantallas/App.jsx
+++ src/pantallas/App.jsx
@@ -4,12 +4,12 @@ import Login from './pantallas/Login';
 import Registro from './pantallas/Registro';
 import Cambiocontraseña from './pantallas/Cambiocontraseña';
 import Presentacion from './pantallas/Presentacion';
-import ImagePost from './pantallas/ImagePost';
 import Guardados from './pantallas/Guardados';
 import EditarPerfil from './EditarPerfil';
 import Mapacentro from './Mapacentro';
 import Paginaprincipal from './PaginaPrincipal';
 import UserProfile from './UserProfile';
+import SocialMediaPost from './pantallas/SocialMediaPost';
 
 
 
@@ -22,13 +22,12 @@ function App() {
         <Route path="/registro" element={<Registro />} />
         <Route path="/Cambiocontraseña" element={<Cambiocontraseña />} />
         <Route path="/Presentacion" element={<Presentacion />} />   
-        <Route path="/ImagePost" element={<ImagePost/>}/>
         <Route path="/Guardados" element={<Guardados/>}/>
         <Route path="/EditarPerfil" element={<EditarPerfil/>}/> 
         <Route path="/Mapacentro" element={<Mapacentro />} />
         <Route path="/Paginaprincipal" element={<Paginaprincipal />} />
         <Route path="/UserProfile" element={<UserProfile />} />
-        
+        <Route path="/SocialMediaPost" element={<SocialMediaPost />} />
       </Routes>
     </div>
   )
