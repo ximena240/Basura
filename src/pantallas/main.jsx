96c5bca71b4a96b9dad1125f6ee92c01d1e036f9
ximena240 <tijerina.almaguer.22067@itsmante.edu.mx>
2025-06-03 11:01:32 -0600

    yadiosito

--- src/pantallas/main.jsx
+++ src/pantallas/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import App from './pantallas/App.jsx';
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -10,4 +10,4 @@ createRoot(document.getElementById('root')).render(
       <App />
     </BrowserRouter>    
   </StrictMode>,
-);
+)
