9d11d3d6b59c629d60b27aeb63b3b589a53c72da
ximena240 <tijerina.almaguer.22067@itsmante.edu.mx>
2025-06-03 08:50:29 -0600

    nnnn

--- src/pantallas/main.jsx
+++ src/pantallas/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css';
+import './index.css'
 import App from './pantallas/App.jsx';
 
 createRoot(document.getElementById('root')).render(
