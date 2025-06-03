8c201fa9cf76f8a42962808c8897ee1fe6e0cd6a
ximena240 <tijerina.almaguer.22067@itsmante.edu.mx>
2025-06-03 15:33:00 -0600

    hhh

--- src/pantallas/Presentacion.jsx
+++ src/pantallas/Presentacion.jsx
@@ -6,7 +6,7 @@ function Presentacion() {
      
       <div className='flex justify-end p-6'>
         <button className='bg-[#164A41] text-white text-sm font-medium rounded-full px-6 py-2 mr-4 hover:bg-[#588157]'>Crear cuenta</button>
-        <button className='bg-[#164A41] text-white text-sm font-medium rounded-full px-6 py-2 mr-4hover:bg-[#588157]'>Iniciar sesión</button>
+        <button className='bg-[#164A41] text-white text-sm font-medium rounded-full px-6 py-2 hover:bg-[#588157]'>Iniciar sesión</button>
       </div>
 
       <div className='flex justify-between items-center h-full ml-20 mr-20 relative'>
@@ -14,17 +14,18 @@ function Presentacion() {
         <div className='flex flex-col justify-center items-start'>
           <h1 className='text-white text-9xl font-extrabold mb-10'>Residuos<br />Red</h1>
           <p className='text-white text-lg font-normal w-96 mb-'>
-            Somos una página web enfocada en la concientización de la gestión de residuos que pueden afectar nuestro entorno.
+            Somos una pagina web enfocada en la concientización de la gestion de residuos que pueden afectar nuestro entorno.
           </p>
         </div>
 
-
-        
-          <img src='/images/logopedorro.png' alt='logopedorro' className='w-[384px] h-[384px] object-contain' />
+      
+        <div className='w-96 h-96 rounded-full bg-white flex items-center justify-center shadow-lg'>
+          <img src='/images/logopedorro.png' alt='logopedorro' className='w-64 h-64 object-contain' />
         </div>
-     </div>
+      </div>
+    </div>
   );
 }
 
-export default Presentacion;  
+export default Presentacion; 
 
